3a4ce3bba726fb527e60ba838d24861a9ddd1b6a
Cristiane Reis <anni.nanne@gmail.com>
2022-05-29 23:33:56 -0300

    serializer

---
+++ livros/serializer.py
@@ -0,0 +1,8 @@
+from .models import Livro
+from rest_framework import serializers
+
+
+class LivrosSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Livro
+        exclude = []
